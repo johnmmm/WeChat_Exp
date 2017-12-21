ef64f8a5833ae3e8c183626f32783ba9f6bedf8c
周京汉 <zhoujh15@mails.tsinghua.edu.cn>
2017-12-21 15:58:51 +0800

    a server that can run ... still need to 'htons'

--- server/server.c
+++ server/server.c
@@ -16,6 +16,7 @@
 #define SIN_PORT 7474
 #define BACKLOG 20
 #define MAX_DATA_SIZE 4096
+#define MAX_NUM 20
 
 struct Users
 {
@@ -37,6 +38,7 @@ int main (int argc, char *argv[])
     char client_name[BACKLOG+1][32];                    //for saving names of clients
     struct Users user;                                  //for recording user's information
     char sendBuf[MAX_DATA_SIZE], receBuf[MAX_DATA_SIZE];//for buffers 
+    int on = 1;                                         //???
 
     if(argc != 2)                                       //command does not have right format
     {
@@ -44,7 +46,7 @@ int main (int argc, char *argv[])
         exit(1);
     }
     strcpy(username, argv[1]);
-    printf("username: %s", username);
+    printf("username: %s\n", username);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);           //try to build a socket(1, socket)
     if(sockfd == -1)
@@ -52,13 +54,16 @@ int main (int argc, char *argv[])
         perror(" failed to build a socket!!\n");
         exit(1);
     }
-    printf("success to build a socket!!!");
+    printf("success to build a socket!!!\n");
+
+    setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on));
 
     serverSockaddr.sin_family = AF_INET;
-    serverSockaddr.sin_port = SIN_PORT;                 //configure my_addr
-    //serverSockaddr.sin_port = htons(SIN_PORT);
-    serverSockaddr.sin_addr.s_addr = INADDR_ANY;
-    //serverSockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //serverSockaddr.sin_port = SIN_PORT;                 //configure my_addr
+    serverSockaddr.sin_port = htons(SIN_PORT);
+    //serverSockaddr.sin_addr.s_addr = INADDR_ANY;
+    serverSockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero(&(serverSockaddr.sin_zero),8);
                                                         //bind it
     int bind_ans = bind(sockfd, (struct sockaddr*)&serverSockaddr, sizeof(struct sockaddr));
     if(bind_ans == -1)
@@ -66,6 +71,7 @@ int main (int argc, char *argv[])
         perror( "failed to bind IP and socket!!!\n");
         exit(1);
     }
+    printf("success to bind! \n");
 
     int listen_ans = listen(sockfd, BACKLOG);           //listen the server
     if(listen_ans == -1)
@@ -73,6 +79,7 @@ int main (int argc, char *argv[])
         perror(" failed to listen server!!!\n");
         exit(1);
     }
+    printf("success to listen!!!\n");
 
     timeout.tv_sec = 1;                                 //refresh after every 1s.
     timeout.tv_usec = 0;
@@ -93,18 +100,26 @@ int main (int argc, char *argv[])
     int select_num = 0;
     while(1)
     {
+        FD_ZERO(&serverfd);                                 //clean it 
+        FD_ZERO(&clientfd);
+        FD_SET(sockfd, &serverfd);
+        
         select_num = select(max_sockfd+1, &serverfd, NULL, NULL, &timeout);
+        //printf("select_num: %d\n", select_num);
         if(select_num == -1)
         {
             perror(" failed to select!!!\n");
-            exit(1);
+            //exit(1);
+            continue;
         }
         else if(select_num == 0)
         {
-            break;
+            //printf("select_num is 0!!\n");
+            continue;
         }
         else
         {
+            printf("else!!\n");
             if(FD_ISSET(sockfd, &serverfd))             //if there is something to 
             {
                 recefd = accept(sockfd, (struct sockaddr*)&clientSockaddr, &sin_size);
@@ -133,9 +148,37 @@ int main (int argc, char *argv[])
                     perror(" failed to send!!!\n");
                 }
                 server_sockfd[client_num] = recefd;
-                strcpy(client_name, user.username);
+                strcpy(client_name[client_num], user.username);
                 client_num++;
                 max_recefd = recefd;
+            }
+        }
+
+        for(int i = 0; i < MAX_NUM; i++)
+        {
+            if(server_sockfd[i] != 0)
+            {
+                FD_SET(server_sockfd[i], &clientfd);
+            }
+        }
+
+        select_num = select(max_recefd+1, &clientfd, NULL, NULL, &timeout);
+        if(select_num == -1)
+        {
+            continue;
+        }
+        else if(select_num == 0)
+        {
+            continue;
+        }
+        else
+        {
+            for(int i = 0; i < client_num; i++)
+            {
+                if(FD_ISSET(recefd, &clientfd))
+                {
+
+                }
             }
         }
     }
